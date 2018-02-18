00b0d461a916ffd783ca55e90862576a77fe161e
prasaria <prasetya.ariawibawa@gmail.com>
2018-02-18 14:07:58 +0700

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -28,6 +29,26 @@ app.get('/todos', (req, res) => {
        res.status(400).send(e);
     });
 });
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }else {
+        Todo.findById(id).then((todo) => {
+            if(!todo){
+               return res.status(404).send();
+            }
+            res.send({todo});
+        }).catch((e) => {
+            res.status(400).send();
+        });
+    }
+
+
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
